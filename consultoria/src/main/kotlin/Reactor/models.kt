dd1aa8a128a2d7448b23857e3f75a4e55dbd4e62
julio96correa <julio96correa@gmail.com>
2025-03-11 23:17:17 -0500

    fix: add pom.xml to consultoria

---
+++ consultoria/src/main/kotlin/Reactor/models.kt
@@ -0,0 +1,7 @@
+package Reactor
+
+data class Costos(val esfuerzo: Double, val viaticos: Double, val infraestructura: Double)
+data class  Gastos(val servicios: Double, val papeleria: Double, val energia: Double)
+data class Riesgos(val porcentaje: Double)
+data class Impuestos(val reteFuente: Double, val reteIca: Double, val iva:Double)
+data class CostoProyecto(val constos: Costos, val gastos: Gastos, val riesgos: Riesgos, val impuestos: Impuestos, val total: Double)
